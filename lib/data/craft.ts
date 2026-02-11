5c944699f3f0fe7782adac0eda4e8fee0d629ed0
pilladipesh33 <pilladipesh.pd@gmail.com>
2026-02-11 18:51:58 +0530

    update card link in craft

--- lib/data/craft.ts
+++ lib/data/craft.ts
@@ -1,56 +1,65 @@
 export const craftData = [
-    {
-        id: 1,
-        xlink: "https://x.com/diip3sh/status/2019428201592995956?s=20",
-        video: "https://mniewerqzpis1qpo.public.blob.vercel-storage.com/craft/shared_layout.mov",
-        alt: "Edit Time"
-    },
-    {
-        id: 2,
-        xlink: "https://x.com/diip3sh/status/2018404162837578147?s=20",
-        video: "https://mniewerqzpis1qpo.public.blob.vercel-storage.com/craft/Shopify%20Transition.mp4",
-        alt: "GLSL Shader Fragment"
-    },
-    {
-        id: 3,
-        xlink: "https://x.com/diip3sh/status/2016955164411023651?s=20",
-        video: "https://mniewerqzpis1qpo.public.blob.vercel-storage.com/craft/slider-gradient-1769668815641.mp4",
-        alt: "Gradient Slider Toggle"
-    },
-    {
-        id: 4,
-        xlink: "https://x.com/diip3sh/status/2013327530997211144?s=20",
-        video: "https://mniewerqzpis1qpo.public.blob.vercel-storage.com/craft/Draggable%20Mask.mp4",
-        alt: "Canvas Masking"
-    },
-    {
-        id: 5,
-        xlink: "https://x.com/diip3sh/status/1997939885471346790?s=20",
-        video: "https://mniewerqzpis1qpo.public.blob.vercel-storage.com/craft/blub-button-1765180414006.mp4",
-        alt: "Light Bulb Button"
-    },
-    {
-        id: 6,
-        xlink: "https://x.com/diip3sh/status/1981019861993365736?s=20",
-        video: "https://mniewerqzpis1qpo.public.blob.vercel-storage.com/craft/lando-norris.mp4",
-        alt: "Threejs Face Masking shader"
-    },
-    {
-        id: 7,
-        xlink: "https://x.com/diip3sh/status/1951916692029755612?s=20",
-        video: "https://mniewerqzpis1qpo.public.blob.vercel-storage.com/craft/twU0vE_GfLprjyGl.mp4",
-        alt: "Shadow Button"
-    },
-    {
-        id: 9,
-        xlink: "https://x.com/diip3sh/status/2000535373404164483?s=20",
-        video: "https://mniewerqzpis1qpo.public.blob.vercel-storage.com/craft/vertical-slider-1765799424856.mp4",
-        alt: "image slider"
-    },
-    {
-        id: 10,
-        xlink: "https://x.com/diip3sh/status/1955337309630443865?s=20",
-        video: "https://mniewerqzpis1qpo.public.blob.vercel-storage.com/craft/blub-button-1765180414006.mp4",
-        alt: "Perpexilty Card"
-    }
-]
+  {
+    id: 1,
+    xlink: "https://x.com/diip3sh/status/2019428201592995956?s=20",
+    video:
+      "https://mniewerqzpis1qpo.public.blob.vercel-storage.com/craft/shared_layout.mov",
+    alt: "Edit Time",
+  },
+  {
+    id: 2,
+    xlink: "https://x.com/diip3sh/status/2018404162837578147?s=20",
+    video:
+      "https://mniewerqzpis1qpo.public.blob.vercel-storage.com/craft/Shopify%20Transition.mp4",
+    alt: "GLSL Shader Fragment",
+  },
+  {
+    id: 3,
+    xlink: "https://x.com/diip3sh/status/2016955164411023651?s=20",
+    video:
+      "https://mniewerqzpis1qpo.public.blob.vercel-storage.com/craft/slider-gradient-1769668815641.mp4",
+    alt: "Gradient Slider Toggle",
+  },
+  {
+    id: 4,
+    xlink: "https://x.com/diip3sh/status/2013327530997211144?s=20",
+    video:
+      "https://mniewerqzpis1qpo.public.blob.vercel-storage.com/craft/Draggable%20Mask.mp4",
+    alt: "Canvas Masking",
+  },
+  {
+    id: 5,
+    xlink: "https://x.com/diip3sh/status/1997939885471346790?s=20",
+    video:
+      "https://mniewerqzpis1qpo.public.blob.vercel-storage.com/craft/blub-button-1765180414006.mp4",
+    alt: "Light Bulb Button",
+  },
+  {
+    id: 6,
+    xlink: "https://x.com/diip3sh/status/1981019861993365736?s=20",
+    video:
+      "https://mniewerqzpis1qpo.public.blob.vercel-storage.com/craft/lando-norris.mp4",
+    alt: "Threejs Face Masking shader",
+  },
+  {
+    id: 7,
+    xlink: "https://x.com/diip3sh/status/1951916692029755612?s=20",
+    video:
+      "https://mniewerqzpis1qpo.public.blob.vercel-storage.com/craft/twU0vE_GfLprjyGl.mp4",
+    alt: "Shadow Button",
+  },
+  {
+    id: 9,
+    xlink: "https://x.com/diip3sh/status/2000535373404164483?s=20",
+    video:
+      "https://mniewerqzpis1qpo.public.blob.vercel-storage.com/craft/vertical-slider-1765799424856.mp4",
+    alt: "image slider",
+  },
+  {
+    id: 10,
+    xlink: "https://x.com/diip3sh/status/1955337309630443865?s=20",
+    video:
+      "https://mniewerqzpis1qpo.public.blob.vercel-storage.com/craft/card-effect-1767077537710.mp4",
+    alt: "Perpexilty Card",
+  },
+];
